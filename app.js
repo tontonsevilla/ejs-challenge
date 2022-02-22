35c4992842b2df7d2b7a57a5fd80c557b863f20e
Winston Sevilla <wsevilla@blastasia.com>
2022-02-22 18:30:53 +0800

    update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -44,7 +45,9 @@ app.get("/compose", function(req, res) {
 app.post("/compose", function(req, res) {
   const post = {
     title: req.body.postTitle,
-    content: req.body.postBody
+    content: req.body.postBody,
+    contentTruncated: _.truncate(req.body.postBody, { length: 100 }),
+    url: `/posts/${_.kebabCase(req.body.postTitle)}`
   };
 
   posts.push(post);
@@ -54,10 +57,14 @@ app.post("/compose", function(req, res) {
 
 // Post Page
 app.get("/posts/:title", function(req, res) {
-  if (posts.some(p => p.title === req.params.title)) {
-    console.log("Is A Match");
+  let post = _.find(posts, function(post) {
+    return _.lowerCase(post.title) === _.lowerCase(req.params.title)
+  });
+
+  if (post != undefined && post != null) {
+    res.render("post", { post: post });
   } else {
-    console.log("Not A Match");
+    res.send("Invalid page.");
   }
 });
 
